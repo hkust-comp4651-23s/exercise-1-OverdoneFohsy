6309baaa57949703109fed3c5360957839241756
Foh Shawn Young <shawn.foh@gmail.com>
2023-03-10 18:26:58 +0800

    CopyFile.java completed.

--- src/main/java/hk/ust/comp4651/CopyFile.java
+++ src/main/java/hk/ust/comp4651/CopyFile.java
@@ -45,7 +45,21 @@ public class CopyFile {
         FSDataOutputStream out = null;
 
         // TODO: Your implementation goes here...
-        
+
+        in  = inFS.open(new Path(src));
+        out = outFS.create(new Path(dst), new Progressable() {
+            /*
+             * Print a dot whenever 64 KB of data has been written to
+             * the datanode pipeline.
+             */
+            public void progress() {
+                System.out.print(".");
+            }
+        });
+
+        System.out.print("\n");
+
+        IOUtils.copyBytes(in, out, BufferSize, true);
     }
 
 }
